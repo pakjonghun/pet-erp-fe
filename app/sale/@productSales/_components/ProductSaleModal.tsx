44078dd4573cafd1427b4bc71d60ccacae61fbde
fireking5997@gmail.com <fireking5997@gmail.com>
2024-04-16 09:10:41 +0900

    판매 현황 판매량순 정렬

--- app/sale/@productSales/_components/ProductSaleModal.tsx
+++ app/sale/@productSales/_components/ProductSaleModal.tsx
@@ -8,6 +8,8 @@ import LabelText from '@/components/ui/typograph/LabelText';
 import { useProductSaleChart } from '@/api/graphql/hooks/product/useProductSaleChart';
 import { LineChart } from '@mui/x-charts/LineChart';
 import dayjs from 'dayjs';
+import CommonLoading from '@/components/ui/loading/CommonLoading';
+import { getKCWFormat, getNumberWithComma } from '@/util';
 
 interface Props {
   selectedProductSale: ProductSaleData;
@@ -34,8 +36,12 @@ const ProductSaleModal: FC<Props> = ({
   const dates = data?.productSale?.map((item) => new Date(item._id).getTime()) ?? [];
   const accProfits = data?.productSale?.map((item) => item.accProfit) ?? [];
   const accPayCosts = data?.productSale?.map((item) => item.accPayCost) ?? [];
+  const clonedClients = clients.map((client) => Object.assign({}, client));
+  clonedClients.sort((a, b) => {
+    if (a.accCount > b.accCount) return -1;
+    else return 1;
+  });
 
-  console.log('data : ', dates);
   return (
     <BaseModal open={open} onClose={onClose}>
       <Typography variant="h6" component="h6" sx={{ mb: 2, fontWeight: 600 }}>
@@ -48,22 +54,26 @@ const ProductSaleModal: FC<Props> = ({
         <LabelText label="리드타임" text={leadTime ?? ''} />
       </Stack>
       <Stack direction="row" flexWrap="wrap" gap={3} mb={3}>
-        <LabelText label="지난주 판매량" text={lastWeek?.accCount ?? ''} />
-        <LabelText label="지난주 매출" text={lastWeek?.accPayCost ?? ''} />
-        <LabelText label="지난주 수익" text={lastWeek?.accProfit ?? ''} />
-        <LabelText label="지난주 평균 매출" text={lastWeek?.averagePayCost ?? ''} />
+        <LabelText label="지난주 판매량" text={getKCWFormat(lastWeek?.accCount ?? 0)} />
+        <LabelText label="지난주 매출" text={getKCWFormat(lastWeek?.accPayCost ?? 0)} />
+        <LabelText label="지난주 수익" text={getKCWFormat(lastWeek?.accProfit ?? 0)} />
+        <LabelText label="지난주 평균 매출" text={getKCWFormat(lastWeek?.averagePayCost ?? 0)} />
       </Stack>
 
       <LabelText label="채널별 판매수량" text={''} />
       <Stack direction="row" flexWrap="wrap" rowGap={1} columnGap={2} mt={1}>
-        {(clients ?? []).map((client) => {
+        {(clonedClients ?? []).map((client) => {
           return (
-            <LabelText key={client._id.mallId} label={client._id.mallId} text={client.accCount} />
+            <LabelText
+              key={client._id.mallId}
+              label={client._id.mallId}
+              text={getNumberWithComma(client.accCount ?? 0)}
+            />
           );
         })}
       </Stack>
       <Box sx={{ bgcolor: 'grey.200', width: '100%', height: 320, color: 'white', mt: 2, pt: 2 }}>
-        {!!data && (
+        {!!data ? (
           <LineChart
             xAxis={[
               {
@@ -92,6 +102,8 @@ const ProductSaleModal: FC<Props> = ({
             }}
             height={300}
           />
+        ) : (
+          <CommonLoading />
         )}
       </Box>
     </BaseModal>
