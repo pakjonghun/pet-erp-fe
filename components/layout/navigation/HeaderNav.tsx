24484dce14db5f8b1a2a5ca4e6a6094dc0157d92
jonghunpak <fireking5997@gmail.co.kr>
2024-04-21 04:03:07 +0900

    테스트 버튼 제거

--- components/layout/navigation/HeaderNav.tsx
+++ components/layout/navigation/HeaderNav.tsx
@@ -1,11 +1,10 @@
 'use client';
 
-import { Button, Stack } from '@mui/material';
+import { Stack } from '@mui/material';
 import { NavMenus } from '../constants';
-import { usePathname, useRouter } from 'next/navigation';
+import { usePathname } from 'next/navigation';
 import { getFirstPath } from '@/util';
 import HeaderMenu from '@/components/ui/listItem/HeaderMenu';
-import { useEffect, useState } from 'react';
 
 const HeaderNav = () => {
   const pathname = usePathname();
@@ -15,12 +14,6 @@ const HeaderNav = () => {
     const firstPath = getFirstPath(pathname);
     return firstPath === path;
   };
-  const [a, setA] = useState(false);
-
-  const rou = useRouter();
-  useEffect(() => {
-    //
-  }, [a, rou]);
 
   return (
     <Stack
@@ -32,15 +25,6 @@ const HeaderNav = () => {
       }}
       direction="row"
     >
-      <Button
-        variant="contained"
-        onClick={() => {
-          rou.replace('/login');
-          setA((prev) => !prev);
-        }}
-      >
-        zzz
-      </Button>
       {navList.map((text) => (
         <HeaderMenu key={text} menuKey={text} selected={getSelected(text)} menu={NavMenus[text]} />
       ))}
