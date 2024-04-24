afa32f672a691784311b7c5db0ec4ce20afa49e7
jonghunpak <fireking5997@gmail.co.kr>
2024-04-24 22:05:10 +0900

    spa config

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   trailingSlash: true,
+  swcMinify: true,
   output: 'export',
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
