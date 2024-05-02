3dbe5311405ce4459fae9b3d353443b15aa69c31
jonghunpak <fireking5997@gmail.co.kr>
2024-05-02 11:29:40 +0900

    도매 탭 추가

---
+++ app/sale/constants.ts
@@ -0,0 +1,8 @@
+export const SaleTabs = {
+  sale: {
+    label: '제품 판매',
+  },
+  'sale/whole-sale': {
+    label: '거래처 판매',
+  },
+};
--- app/sale/layout.tsx
+++ app/sale/layout.tsx
@@ -2,26 +2,40 @@
 
 import { FC, ReactNode, useEffect, useState } from 'react';
 import SubHeader from '@/components/layout/header/SubHeader';
-import { Box, Stack } from '@mui/material';
+import { Box, Stack, Tab, Tabs } from '@mui/material';
 import SwitchDate from '@/components/calendar/dateSwitch/SwitchDate';
 import { SearchStandard } from '@/components/calendar/dateSwitch/types';
 import { DateRange } from '@/components/calendar/dateFilter/type';
 import { useReactiveVar } from '@apollo/client';
 import { clientTotal, saleRange, saleTotal } from '@/store/saleStore';
 import { getToday } from '@/components/calendar/dateFilter/utils';
+import { usePathname } from 'next/navigation';
+import { SaleTabs } from './constants';
+import { getOriginPath } from '@/utils/common';
+import Link from 'next/link';
 interface Props {
   productSales: ReactNode;
   topClients: ReactNode;
 }
 
 const SaleLayout: FC<Props> = ({ productSales, topClients }) => {
+  const pathname = usePathname();
+  const tabs = Object.keys(SaleTabs) as (keyof typeof SaleTabs)[];
+  const currentTabIndex = tabs.findIndex((item) => {
+    return item === getOriginPath(pathname);
+  });
+
   const range = useReactiveVar(saleRange);
   const setRange = (value: DateRange) => saleRange(value);
   const [searchStandard, setSearchStandard] = useState<SearchStandard>('일');
 
+  const handleResetDateRange = () => {
+    saleRange(getToday());
+  };
+
   useEffect(() => {
     return () => {
-      saleRange(getToday());
+      handleResetDateRange();
       clientTotal({ totalCount: 0, totalPayCost: 0, totalProfit: 0 });
       saleTotal({ totalCount: 0, totalPayCost: 0, totalProfit: 0 });
     };
@@ -29,9 +43,38 @@ const SaleLayout: FC<Props> = ({ productSales, topClients }) => {
 
   return (
     <Box sx={{ height: '100%' }}>
-      <SubHeader sx={{ boxShadow: 'none' }} title="판매" />
+      <SubHeader title="판매">
+        <Tabs
+          sx={{ mt: 2 }}
+          value={currentTabIndex == -1 ? 0 : currentTabIndex}
+          indicatorColor="primary"
+        >
+          {tabs.map((tab) => {
+            const tabItem = SaleTabs[tab];
+            return (
+              <Tab
+                sx={{
+                  transition: 'all .3s',
+                  fontSize: 16,
+                  '&:hover': {
+                    bgcolor: (theme) => theme.palette.action.selected,
+                  },
+                  '&.Mui-selected': {
+                    fontWeight: 800,
+                  },
+                }}
+                href={`/${tab}`}
+                component={Link}
+                label={tabItem.label}
+                key={tab}
+                onClick={handleResetDateRange}
+              />
+            );
+          })}
+        </Tabs>
+      </SubHeader>
       <SwitchDate
-        sx={{ pl: 3 }}
+        sx={{ pl: 3, mt: 2 }}
         range={range}
         setRange={setRange}
         searchStandard={searchStandard}
