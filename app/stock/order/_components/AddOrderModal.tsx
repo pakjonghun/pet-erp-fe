f52c275d156ac40ae5f04ee03fff9c672f2c42f1
pakjonghun <150112208+pakpakjonghun@users.noreply.github.com>
2024-05-08 10:57:57 +0900

    autocomplete 제외한 나머지 발주 등록 input폼 작성

--- app/stock/order/_components/AddOrderModal.tsx
+++ app/stock/order/_components/AddOrderModal.tsx
@@ -12,7 +12,7 @@ import {
   Typography,
 } from '@mui/material';
 import { FC } from 'react';
-import { Controller, useForm } from 'react-hook-form';
+import { Controller, useFieldArray, useForm } from 'react-hook-form';
 import {
   CreateClientForm,
   createClientSchema,
@@ -27,6 +27,27 @@ import { filterEmptyValues } from '@/utils/common';
 import { clientTypes } from '../constants';
 import NumberInput from '@/components/ui/input/NumberInput';
 import { CLIENT_PREFIX } from '@/constants';
+import {
+  CreateOrderForm,
+  createOrderSchema,
+} from '../_validations/createOrderValidation';
+
+const factories = [
+  {
+    _id: '123',
+    name: 'Central Warehouse',
+    phoneNumber: '123-456-7890',
+    address: '123 Central Ave, Big City',
+    note: 'Main distribution center',
+  },
+  {
+    _id: '1234',
+    name: 'East Side Storage',
+    phoneNumber: '987-654-3210',
+    address: '456 East St, Capital City',
+    note: 'Handles east region deliveries',
+  },
+];
 
 interface Props {
   open: boolean;
@@ -41,44 +62,47 @@ const AddOrderModal: FC<Props> = ({ open, onClose }) => {
     control,
     handleSubmit,
     formState: { errors },
-  } = useForm<CreateClientForm>({
-    resolver: zodResolver(createClientSchema),
+  } = useForm<CreateOrderForm>({
+    resolver: zodResolver(createOrderSchema),
     defaultValues: {
-      code: '',
-      name: '',
-      clientType: ClientType.Bender,
-      businessName: '',
-      businessNumber: '',
-      manager: '',
-      managerTel: '',
-      inActive: true,
+      count: 0,
+      factory: '',
+      products: [],
+      payCost: 0,
+      notPayCost: 0,
+      totalPayCost: 0,
     },
   });
 
-  const onSubmit = (values: CreateClientForm) => {
-    const newValues = filterEmptyValues(values) as CreateClientForm;
-    createClient({
-      variables: {
-        createClientInput: {
-          ...newValues,
-          feeRate: values.feeRate == null ? null : values.feeRate / 100,
-        },
-      },
-      onCompleted: () => {
-        snackMessage({
-          message: '거래처등록이 완료되었습니다.',
-          severity: 'success',
-        });
-        handleClose();
-      },
-      onError: (err) => {
-        const message = err.message;
-        snackMessage({
-          message: message ?? '거래처등록이 실패했습니다.',
-          severity: 'error',
-        });
-      },
-    });
+  const { replace, append, remove, fields } = useFieldArray({
+    control,
+    name: 'products',
+  });
+
+  const onSubmit = (values: CreateOrderForm) => {
+    const newValues = filterEmptyValues(values) as CreateOrderForm;
+    // createClient({
+    //   variables: {
+    //     createClientInput: {
+    //       ...newValues,
+    //       feeRate: values.feeRate == null ? null : values.feeRate / 100,
+    //     },
+    //   },
+    //   onCompleted: () => {
+    //     snackMessage({
+    //       message: '거래처등록이 완료되었습니다.',
+    //       severity: 'success',
+    //     });
+    //     handleClose();
+    //   },
+    //   onError: (err) => {
+    //     const message = err.message;
+    //     snackMessage({
+    //       message: message ?? '거래처등록이 실패했습니다.',
+    //       severity: 'error',
+    //     });
+    //   },
+    // });
   };
 
   const handleClose = () => {
@@ -89,187 +113,57 @@ const AddOrderModal: FC<Props> = ({ open, onClose }) => {
   return (
     <BaseModal open={open} onClose={handleClose}>
       <Typography variant="h6" component="h6" sx={{ mb: 2, fontWeight: 600 }}>
-        거래처 입력
+        발주 등록
       </Typography>
       <form onSubmit={handleSubmit(onSubmit)}>
-        <Stack
-          direction="row"
-          justifyContent="space-between"
-          alignItems="center"
-          gap={3}
-          sx={{ mb: 3 }}
-        >
-          <Typography>새로운 거래처을 입력합니다.</Typography>
-          <Controller
-            control={control}
-            name="inActive"
-            render={({ field }) => {
-              return (
-                <FormControlLabel
-                  sx={{ width: 'fit-content' }}
-                  control={<Switch defaultChecked {...field} />}
-                  label={field.value ? '거래중' : '거래종료'}
-                />
-              );
-            }}
-          />
-        </Stack>
+        <Typography>새로운 발주를 등록합니다.</Typography>
 
         <FormGroup sx={modalSizeProps}>
           <Controller
             control={control}
-            name="code"
-            render={({ field }) => (
-              <FormControl required>
+            name="payCost"
+            render={({ field }) => {
+              return (
                 <TextField
                   {...field}
+                  sx={{ mt: 4 }}
+                  label="계약금"
                   size="small"
-                  required
-                  label="거래처코드"
-                  error={!!errors.code?.message}
-                  helperText={errors.code?.message ?? ''}
-                  InputProps={{
-                    startAdornment: (
-                      <InputAdornment position="start">{`${CLIENT_PREFIX} - `}</InputAdornment>
-                    ),
-                  }}
+                  error={!!errors.payCost?.message}
+                  helperText={errors.payCost?.message ?? ''}
                 />
-              </FormControl>
-            )}
+              );
+            }}
           />
           <Controller
             control={control}
-            name="name"
-            render={({ field }) => (
-              <FormControl required>
+            name="notPayCost"
+            render={({ field }) => {
+              return (
                 <TextField
-                  size="small"
                   {...field}
-                  required
-                  label="거래처이름"
-                  error={!!errors.name?.message}
-                  helperText={errors.name?.message ?? ''}
+                  label="잔금"
+                  size="small"
+                  error={!!errors.notPayCost?.message}
+                  helperText={errors.notPayCost?.message ?? ''}
                 />
-              </FormControl>
-            )}
+              );
+            }}
           />
           <Controller
             control={control}
-            name="feeRate"
-            render={({ field }) => (
-              <FormControl>
-                <NumberInput
-                  field={field}
-                  label="수수료 비율(0~100사이 숫자)"
-                  error={!!errors.feeRate?.message}
-                  helperText={errors.feeRate?.message ?? ''}
-                  endAdornment={
-                    <InputAdornment position="end">%</InputAdornment>
-                  }
-                />
-              </FormControl>
-            )}
-          />
-          <Controller
-            control={control}
-            name="clientType"
-            render={({ field }) => (
-              <Autocomplete
-                disablePortal
-                filterSelectedOptions
-                value={field.value}
-                onChange={(_, value) => field.onChange(value)}
-                noOptionsText="검색결과가 없습니다."
-                options={Object.keys(clientTypes)}
-                getOptionLabel={(option) => {
-                  return clientTypes[option as unknown as ClientType] ?? '';
-                }}
-                renderInput={(params) => (
-                  <FormControl fullWidth>
-                    <TextField
-                      {...params}
-                      size="small"
-                      {...field}
-                      label="거래처 형태"
-                      error={!!errors.clientType?.message}
-                      helperText={errors.clientType?.message ?? ''}
-                    />
-                  </FormControl>
-                )}
-              />
-            )}
-          />
-          <Controller
-            control={control}
-            name="businessName"
-            render={({ field }) => (
-              <FormControl>
+            name="totalPayCost"
+            render={({ field }) => {
+              return (
                 <TextField
-                  size="small"
                   {...field}
-                  label="거래처 상호"
-                  error={!!errors.businessName?.message}
-                  helperText={errors.businessName?.message ?? ''}
-                />
-              </FormControl>
-            )}
-          />
-          <Controller
-            control={control}
-            name="businessNumber"
-            render={({ field }) => (
-              <FormControl>
-                <TextField
+                  label="총 금액"
                   size="small"
-                  {...field}
-                  label="사업자 등록번호"
-                  error={!!errors.businessNumber?.message}
-                  helperText={errors.businessNumber?.message ?? ''}
+                  error={!!errors.totalPayCost?.message}
+                  helperText={errors.totalPayCost?.message ?? ''}
                 />
-              </FormControl>
-            )}
-          />
-          <Controller
-            control={control}
-            name="payDate"
-            render={({ field }) => (
-              <NumberInput
-                field={field}
-                label="결제일(매달 결제할 날짜(1~31사이 값을 입력)"
-                error={!!errors.payDate?.message}
-                helperText={errors.payDate?.message ?? ''}
-              />
-            )}
-          />
-          <Controller
-            control={control}
-            name="manager"
-            render={({ field }) => (
-              <FormControl>
-                <TextField
-                  size="small"
-                  {...field}
-                  label="관리자 이름"
-                  error={!!errors.manager?.message}
-                  helperText={errors.manager?.message ?? ''}
-                />
-              </FormControl>
-            )}
-          />
-          <Controller
-            control={control}
-            name="manager"
-            render={({ field }) => (
-              <FormControl>
-                <TextField
-                  size="small"
-                  {...field}
-                  label="연락처"
-                  error={!!errors.managerTel?.message}
-                  helperText={errors.managerTel?.message ?? ''}
-                />
-              </FormControl>
-            )}
+              );
+            }}
           />
         </FormGroup>
         <Stack direction="row" gap={1} sx={{ mt: 3 }} justifyContent="flex-end">
--- app/stock/order/_validations/createOrderValidation.ts
+++ app/stock/order/_validations/createOrderValidation.ts
@@ -9,6 +9,9 @@ export const createOrderSchema = z.object({
   factory: z.string().min(1, { message: '발주할 공장을 선택하세요.' }),
   products: z.array(createOrderProductSchema),
   count: z.number().min(1, { message: '1개 이상의 제품을 입력해주세요.' }),
+  payCost: z.number().min(1, { message: '1 이상의 숫자를 입력하세요.' }),
+  notPayCost: z.number().min(1, { message: '1 이상의 숫자를 입력하세요.' }),
+  totalPayCost: z.number().min(1, { message: '1 이상의 숫자를 입력하세요.' }),
 });
 
 export type CreateOrderForm = z.infer<typeof createOrderSchema>;
