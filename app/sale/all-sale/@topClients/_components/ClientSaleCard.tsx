508007f3119cefa499d5f39da417be1708c22f18
fireking5997@gmail.com <fireking5997@gmail.com>
2024-06-21 19:27:25 +0900

    판매 수익 비교 기능이 거래처와 판매에 모두 적용되도록 수정

--- app/sale/all-sale/@topClients/_components/ClientSaleCard.tsx
+++ app/sale/all-sale/@topClients/_components/ClientSaleCard.tsx
@@ -3,6 +3,8 @@ import { Box, Paper, Typography } from '@mui/material';
 import { SaleInfos } from '@/http/graphql/codegen/graphql';
 import SaleCard from '@/components/card/SaleCard';
 import { getProfitRate } from '@/utils/sale';
+import { useReactiveVar } from '@apollo/client';
+import { showPrevSaleData } from '@/store/saleStore';
 
 interface Props {
   clientSaleData: SaleInfos;
@@ -10,27 +12,32 @@ interface Props {
 }
 
 const ClientSaleCard: FC<Props> = ({ clientSaleData, scrollRef }) => {
+  const isShowPrevData = useReactiveVar(showPrevSaleData);
   return (
     <Paper ref={scrollRef} sx={{ position: 'relative', py: 3, px: 4 }}>
       <Box>
         <Typography>{clientSaleData.name}</Typography>
         <SaleCard
+          isShowPrevData={isShowPrevData}
           label="판매수량"
           current={clientSaleData?.accCount ?? 0}
           previous={clientSaleData?.prevAccCount ?? 0}
           numberType="comma"
         />
         <SaleCard
+          isShowPrevData={isShowPrevData}
           label="매출"
           current={clientSaleData?.accPayCost ?? 0}
           previous={clientSaleData?.prevAccPayCost ?? 0}
         />
         <SaleCard
+          isShowPrevData={isShowPrevData}
           label="수익"
           current={clientSaleData?.accProfit ?? 0}
           previous={clientSaleData?.prevAccProfit ?? 0}
         />
         <SaleCard
+          isShowPrevData={isShowPrevData}
           label="수익율"
           numberType="percent"
           current={getProfitRate(clientSaleData?.accProfit ?? 0, clientSaleData?.accPayCost ?? 0)}
